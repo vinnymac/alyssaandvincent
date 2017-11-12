ce3373cdc0db73cb697fd0b3acdd17639631c760
Vincent Taverna <vinny@getlua.com>
2017-11-12 17:12:56 -0500

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-4ebc29cfbfb626391b40.js","dc7112b5137e794a0f813fa5cfc152ce"],["/commons-0bd2e762d8d2f9bbe398.js","0286721dcaabccef4c3713521b957d5f"],["/index.html","3ccb47e664d2a45a9bf67e7761f977ac"],["/manifest.json","1ef55b13618a65e49916a13310a611cb"],["/offline-plugin-app-shell-fallback/index.html","b154d7b466a01a9abce0f196fcef8fc5"]];
+var precacheConfig = [["/app-4ebc29cfbfb626391b40.js","dc7112b5137e794a0f813fa5cfc152ce"],["/app-a958e7c61e53908dd010.js","531804eb01b5577454518e5f9d4f6d6b"],["/commons-0bd2e762d8d2f9bbe398.js","0286721dcaabccef4c3713521b957d5f"],["/index.html","556e0c93bfcb93ee5a5200d814509a4f"],["/manifest.json","1ef55b13618a65e49916a13310a611cb"],["/offline-plugin-app-shell-fallback/index.html","37f242941b6cb0df25fc4aae0d1b80c4"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
